c68f6d01a38ca5359bc478a07daa03c9485ec1cf
Matthias Georgi <matthias@soundcloud.com>
2014-09-10 12:36:27 +0200

    fix terminal background

--- personal/setup-theme.el
+++ personal/setup-theme.el
@@ -1,5 +1,13 @@
 (require-package 'color-theme)
 
+(defun on-after-init ()
+    (unless (display-graphic-p (selected-frame))
+          (set-face-background 'default "unspecified-bg" (selected-frame))))
+
+(add-hook 'window-setup-hook 'on-after-init)
+
+(add-hook 'server-visit-hook 'on-after-init)
+
 (require 'color-theme-solarized)
 
 (load-theme 'solarized-dark 1)
